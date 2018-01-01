0dc90d474e5d7ad3a19ec96449aa5a3197e7fca4
jdiin-ssd <jdiin-ssd@DESKTOP-U343RPE>
2018-01-01 18:05:02 +0800

    111

--- MatchApp/src/WebAPI/Utility.java
+++ MatchApp/src/WebAPI/Utility.java
@@ -1,7 +1,11 @@
 package WebAPI;
 
 import java.util.ArrayList;
+import java.util.Properties;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -37,6 +41,86 @@ public class Utility {
 		return locations;
 	}
 	
-	
+	public static void main(String [] args) {    
+	      // Recipient's email ID needs to be mentioned.
+	      String to = "jdiin@hotmail.com";
+
+	      // Sender's email ID needs to be mentioned
+	      //String from = "web@gmail.com";
+
+	      // Assuming you are sending email from localhost
+	      String host = "smtp.gmail.com";
+	      int port = 587;
+	      final String username = "Dasao@gmail.com";
+	      final String password = "";//your password
+
+	      // Get system properties
+	      Properties properties = System.getProperties();
+
+	      
+	      properties.put("mail.smtp.host", host);
+	      properties.put("mail.smtp.auth", "true");
+	      properties.put("mail.smtp.starttls.enable", "true");
+	      properties.put("mail.smtp.port", port);
+//	      Session session = Session.getInstance(properties, new Authenticator() {
+//	       protected PasswordAuthentication getPasswordAuthentication() {
+//	        return new PasswordAuthentication(username, password);
+//	       }
+//	      });
+	      
+	      Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+	    	    protected PasswordAuthentication getPasswordAuthentication() {
+	    	        return new PasswordAuthentication(username, password);
+	    	    }
+	    	});
+	      
+	      try {
+
+	    	   Message message = new MimeMessage(session);
+	    	   message.setFrom(new InternetAddress("fromn@gmail.com"));
+	    	   message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+	    	   message.setSubject("測試寄信.");
+	    	   message.setText("Dear Levin, \n\n 測試 測試 測試 測試 測試 測試 email !");
+
+	    	   Transport transport = session.getTransport("smtp");
+	    	   transport.connect(host, port, username, password);
+
+	    	   Transport.send(message);
+
+	    	   System.out.println("寄送email結束.");
+
+	    	  } catch (MessagingException e) {
+	    	   throw new RuntimeException(e);
+	    	  }
+	    	 
+	      // Setup mail server
+	      //properties.setProperty("mail.smtp.host", host);
+
+	      // Get the default Session object.
+	      //Session session = Session.getDefaultInstance(properties);
+
+//	      try {
+//	         // Create a default MimeMessage object.
+//	         MimeMessage message = new MimeMessage(session);
+//
+//	         // Set From: header field of the header.
+//	         message.setFrom(new InternetAddress(from));
+//
+//	         // Set To: header field of the header.
+//	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//
+//	         // Set Subject: header field
+//	         message.setSubject("This is the Subject Line!");
+//
+//	         // Now set the actual message
+//	         message.setText("This is actual message");
+//
+//	         // Send message
+//	         Transport.send(message);
+//	         System.out.println("Sent message successfully....");
+//	      } catch (MessagingException mex) {
+//	         mex.printStackTrace();
+//	      }
+	   }
 
 }
